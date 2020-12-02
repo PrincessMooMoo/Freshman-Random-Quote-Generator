2348a5793558f6a23cd85c185733eab1c96255a7
Wendy C <chriscowendy@gmail.com>
2020-12-02 09:58:32 -0800

    added a loading animation

--- js/script.js
+++ js/script.js
@@ -1,9 +1,13 @@
+const spinner = document.querySelector('#js-spinner');
+
 const newQuoteButton = document.querySelector('#js-new-quote');
 newQuoteButton.addEventListener('click', getQuote);
 
 const endpoint = 'https://api.whatdoestrumpthink.com/api/v1/quotes/random';
 
 async function getQuote() {
+    spinner.classList.remove('hidden');
+    newQuoteButton.disabled = true;
     try {
         const response = await fetch(endpoint)
         if (!response.ok){
@@ -12,8 +16,10 @@ async function getQuote() {
         const json = await response.json();
         displayQuote(json.message);
     } catch (err) {
-        console.log(err)
         alert('Failed to fetch new quote');
+    } finally {
+        newQuoteButton.disabled = false;
+        spinner.classList.add('hidden');
     }
 }
 
